c5f5b19bbd220024ed6d4f46ddecafa2266124ab
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-04-07 16:17:24 +0200

    commented Rook class

--- Components/Models/Rook.cs
+++ Components/Models/Rook.cs
@@ -9,14 +9,20 @@ namespace Chess.Models
             identifier = 'R';
         }
 
+        /* This method overrides the relative method defined in the parent class
+        by applying specific rules on the possibility of doing certain kinds of moves
+        belonging to the Rook piece. */
         public override bool CheckMove(Position end)
         {
             bool result = false;
 
-            if (start.X == end.X) // vertical move
+            // If is a vertical move
+            if (start.X == end.X)
             {
                 result = true;
 
+                /* Verifying that in the route, whether you move
+                upwards or downwards, all the cells are empty. */
                 for (int i = 1; ((i < GetMoveAbsValue(end, 'Y')) && (result)); i++)
                 {
                     if (start.Y < end.Y)
@@ -29,10 +35,13 @@ namespace Chess.Models
                     }
                 }
             }
-            else if (start.Y == end.Y) // horizontal move
+            // If is an horizontal move
+            else if (start.Y == end.Y)
             {
                 result = true;
 
+                /* Verifying that in the route, whether you move
+                left or right, all the cells are empty. */
                 for (int i = 1; ((i < GetMoveAbsValue(end, 'X')) && (result)); i++)
                 {
                     if (start.X < end.X)
