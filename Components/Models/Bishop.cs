26b11ba2e0bb7e24fdbd001529b03fd0c4f9a258
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-04-07 15:11:49 +0200

    commented Bishop class

--- Components/Models/Bishop.cs
+++ Components/Models/Bishop.cs
@@ -9,19 +9,23 @@ namespace Chess.Models
             identifier = 'B';
         }
 
+        /* This method overrides the relative method defined in the parent class
+        by applying specific rules on the possibility of doing certain kinds of moves
+        belonging to the Bishop piece. */
         public override bool CheckMove(Position end)
         {
             bool result = false;
 
+            /* Getting how many cells the piece is moved horizontally and vertically */
             int absX = GetMoveAbsValue(end, 'X');
             int absY = GetMoveAbsValue(end, 'Y');
 
-            // diagonal move
+            // If is a diagonal move
             if (absX == absY)
             {
                 result = true;
 
-                // check if all empty cells on the way
+                // I can go on as long as there are empty cells in my way
                 for (int i = 1; ((i < absX) && (result)); i++)
                 {
                     if (start.X < end.X)
@@ -39,6 +43,7 @@ namespace Chess.Models
                 }
             }
 
+            /* Checking that there isn't already a piece of mine in the place where I want to move. */
             return result ? IsPositionFreeOfAllies(end) : result;
         }
     }
