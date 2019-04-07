1894aee349336d2852d681cfccdd1861b44459d4
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-04-07 15:21:10 +0200

    commented Knight class

--- Components/Models/Knight.cs
+++ Components/Models/Knight.cs
@@ -6,14 +6,20 @@ namespace Chess.Models
     {
         public Knight(bool isWhite, int x, int y, Piece[,] matrix) : base(isWhite, x, y, matrix)
         {
-            identifier = '♞'; // horse
+            identifier = 'H'; // horse
         }
 
+        /* This method overrides the relative method defined in the parent class
+        by applying specific rules on the possibility of doing certain kinds of moves
+        belonging to the Knight piece. */
         public override bool CheckMove(Position end)
         {
+            /* Getting how many cells the piece is moved horizontally and vertically */
             int absX = GetMoveAbsValue(end, 'X');
             int absY = GetMoveAbsValue(end, 'Y');
-
+            
+            /* Check that the piece performs two horizontal/vertical steps followed by a
+            vertical/horizontal step, so that the path traveled ideally forms an "L". */
             return ((absX == 2) && (absY == 1)
                     || (absX == 1) && (absY == 2)) ? 
                 IsPositionFreeOfAllies(end) : false;
